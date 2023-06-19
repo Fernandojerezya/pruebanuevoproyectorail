600b6b9c95cd071fc7259ef819e6d3900888b933
fernandojerez <fernandojerezy@gmail.com>
2023-06-19 11:58:08 -0400

    Se crearon las 3 rutas ,su html y visualizador

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'contact/index'
+  get 'projects/index'
+  get 'home/index'
+  root 'home#index'
+  get '/home', to: 'home#index'
+  get '/projects', to: 'projects#index'
+  get '/contact', to: 'contact#index'
 end
